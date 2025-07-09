8ab14c33809ef153df6c8392ffba0caabbf0cf8e
grelinfo <loic.gremaud@grelinfo.ch>
2025-07-09 16:17:21 +0200

    🐛 Fix log formatting for server startup message

--- src/server.rs
+++ src/server.rs
@@ -6,7 +6,7 @@ use tokio_util::sync::CancellationToken;
 /// Run the server with the given configuration and shutdown token
 pub async fn server(config: Config, shutdown_token: CancellationToken) {
     let container = Container::new(config);
-    log::info!("{APP_NAME} v{APP_VERSION} started");
+    log::info!("{APP_NAME} (v{APP_VERSION}) started");
     container.solar_service().run(shutdown_token).await;
     container.shutdown().await;
 }
